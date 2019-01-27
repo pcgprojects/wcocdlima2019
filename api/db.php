a454bc82ebdfc1489557903c4cde2d8e6d83d90d
Roy Cespedes <u201401977@upc.edu.pe>
2019-01-27 15:29:29 -0500

    add validacion tipo de pago

--- api/db.php
+++ api/db.php
@@ -42,6 +42,117 @@ function insertArr($tableName, $insData)
 }
 
 
+function sendEmailClienteVisa($data){
+	
+	$host = 'single-priva8.privatednsorg.com';
+	$subject = 'Regiser Form 14th World Congress of Cosmetic Dermatology';
+    $para = $data['email'];
+	$password = 'C0ngr3ss.2019';
+
+    //Componemos cuerpo correo.
+    $msjCorreo .= "<strong>MESSAGE SENT FROM THE REGISTER US FORM.</strong> ";
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Title:</strong> " . $data['title'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>First name:</strong> " . $data['first_name'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Last name:</strong> " . $data['last_name'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Company:</strong> " . $data['company'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Address:</strong> " . $data['address'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Country:</strong> " . $data['country'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>City:</strong> " . $data['city'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Email:</strong> " . $data['email'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Phone:</strong> " . $data['phone'];
+    $msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Fee:</strong> " . $data['fee'];
+	$msjCorreo .= "<br><br>";
+    $msjCorreo .= "<strong>Amount:</strong> USD " . $data['amount'];
+	$msjCorreo .= "<br><br>";
+	$pagoonline = parseInt($data['pagoonline']);
+	if($pagoonline === 1){
+		$msjCorreo .= "<strong>Método de pago:</strong> Pago en Línea con Visa ";
+		$msjCorreo .= "<br><br>";
+	}else if($pagoonline === 2){
+		$msjCorreo .= "<strong>Método de pago:</strong> Pago en Línea con MasterCard ";
+		$msjCorreo .= "<br><br>";
+	}else if($pagoonline === 3){
+		$msjCorreo .= "<strong>Método de pago:</strong> Depósito o Transferencia Nacional ";
+		$msjCorreo .= "<br><br>";
+	}else{
+		$msjCorreo .= "<strong>Método de pago:</strong> Depósito o Transferencia Internacional ";
+		$msjCorreo .= "<br><br>";
+	}
+	if($pagoonline === 1){
+		//Visa
+		$msjCorreo .= "<p>Pago rápido y seguro con tarjeta de Crédito o Débito VISA.</p>" ;
+		$msjCorreo .= "<p>Como último paso para su inscripción, VISA le enviará un mail de confirmación, dicho mail debe ser reenviado al correo informes3@grupomilenium.pe</p>" ;
+		$msjCorreo .= "<p>Cualquier consulta o duda, favor de contactarse al Whatsapp: +51 981 502 034 / Teléfono: +511 422 4591.</p>" ;
+		$msjCorreo .= "<p><strong>Términos y Condiciones:</strong></p>" ;
+		$msjCorreo .= "<p>No se aceptan cambios ni devoluciones una vez realizado el pago.</p>" ;
+	}else if($pagoonline === 2){
+		//Mastercard
+		$msjCorreo .= "<p>Pago rápido y seguro con tarjeta de Crédito o Débito MasterCard.</p>" ;
+		$msjCorreo .= "<p>Complete los campos solicitados y el monto según su categoría.</p>" ;
+		$msjCorreo .= "<p>Como últimos pasos para su inscripción, le enviaremos un mail dónde estará disponible el enlace para completar su pago con tarjetas MasterCard.</p>" ;
+		$msjCorreo .= "<p><strong>MasterCard</strong> le enviará un mail de confirmación, dicho mail debe ser reenviado al correo informes3@grupomilenium.pe</p>" ;
+		$msjCorreo .= "<p>Cualquier consulta o duda, favor de contactarse al Whatsapp: +51 981 502 034 / Teléfono: +511 422 4591.</p>" ;
+		$msjCorreo .= "<p><strong>Términos y Condiciones:</strong></p>" ;
+		$msjCorreo .= "<p>No se aceptan cambios ni devoluciones una vez realizado el pago.</p>" ;
+	}else if($pagoonline === 3){
+		//Depósito Nacional
+		$msjCorreo .= "<p>Nombre de Banco: <strong>BBVA CONTINENTAL</strong></p>" ;
+		$msjCorreo .= "<p>N° Cta. Cte. Dólares: <strong>0011-0370-0100023817-48</strong></p>" ;
+		$msjCorreo .= "<p>N° Cta. Interbancaria CCI: <strong>011-370-000100023817-48</strong></p>" ;
+		$msjCorreo .= "<p>A nombre de: <strong>GRUPO PERUANO DE TERAPEUTICA DERMATOLOGICA, ESTETICA Y LASER</strong></p>" ;
+		$msjCorreo .= "<p>RUC: <strong>20554571159</strong></p>" ;
+		$msjCorreo .= "<p>Cualquier consulta o duda, favor de contactarse al Whatsapp: +51 981 502 034 / Teléfono: +511 422 4591.</p>" ;
+		$msjCorreo .= "<p><strong>Términos y Condiciones:</strong></p>" ;
+		$msjCorreo .= "<p>No se aceptan cambios ni devoluciones una vez realizado el pago.</p>" ;
+	}else{
+		//Depósito Internacional
+		$msjCorreo .= "<p>Nombre de Banco: <strong>BBVA CONTINENTAL</strong></p>" ;
+		$msjCorreo .= "<p>N° Cta. Cte. Dólares: <strong>0011-0370-0100023817-48</strong></p>" ;
+		$msjCorreo .= "<p>Código SWIFT: <strong>BCONPEPL</strong></p>" ;
+		$msjCorreo .= "<p>A nombre de: <strong>GRUPO PERUANO DE TERAPEUTICA DERMATOLOGICA, ESTETICA Y LASER</strong></p>" ;
+		$msjCorreo .= "<p>RUC: <strong>20554571159</strong></p>" ;
+		$msjCorreo .= "<p>Dirección: <strong>Av. Rivera Navarrete 451 Oficina 601 - San Isidro</strong></p>" ;
+		$msjCorreo .= "<p>Teléfono: <strong>(511) 422 -4591</strong></p>" ;
+		$msjCorreo .= "<p>Cualquier consulta o duda, favor de contactarse al Whatsapp: +51 981 502 034 / Teléfono: +511 422 4591.</p>" ;
+		$msjCorreo .= "<p><strong>Términos y Condiciones:</strong></p>" ;
+		$msjCorreo .= "<p>No se aceptan cambios ni devoluciones una vez realizado el pago.</p>" ;
+	}
+
+		try {
+				$mail = new PHPMailer();
+				$mail->IsSMTP();
+				$mail->SMTPAuth = true;
+				$mail->Host = $host;
+				$mail->Username = $para;
+				$mail->Password = $password;
+				$mail->From = $para;
+				$mail->FromName = "WCOCD LIMA 2019";
+				$mail->AddAddress($para);
+				$mail->addBCC("ocori.reyes@gmail.com");
+				$mail->Subject = $subject;
+				$mail->Body = utf8_decode($msjCorreo);
+				$mail->IsHTML(true);
+			
+
+				$mail->Send();
+				return true;
+			} catch (phpmailerException $e) {
+				return false;
+			} catch (Exception $e) {
+				return false;
+			}
+}
+
 function sendEmailConfirm2($data){
 	
 	$host = 'single-priva8.privatednsorg.com';
